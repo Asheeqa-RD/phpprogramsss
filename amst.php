2a9381848cdf64f179b936a3e41a39371d2b65ea
Asheeqa-RD <asheeqakp123@gmail.com>
2024-11-01 12:16:07 +0530

    initial

--- amst.php
+++ amst.php
@@ -6,6 +6,32 @@
     <title>Document</title>
 </head>
 <body>
-    <p>gdgdugsd</p>
+    <h1>Amstrong number</h1>
+    <form method="get">
+        <label>Enter the number</label>
+        <input type ="number" name="num">
+        <button  type="submit"> Check Number</button>
+    </form> 
+
+    <?php 
+    if($_GET){
+        $num=$_GET['num'];
+        $sum=0;
+        $n=strlen(string)$num;
+        $number=$num;
+
+        while($num!=0){
+            $rem=$num % 10;
+            $sum=$sum+pow($rem,$n);
+            $num = ($num/10);
+        }
+
+        if($num==$number){
+            echo "$number is an amstrong number"
+        }else{
+             echo "$number is  not an amstrong number"
+        }
+    }
+    
 </body>
 </html>
